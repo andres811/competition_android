da6360a3cf9e8cdf03f469ec8fccb517df1f2f78
Andrés Peña <apena@cualit.com>
2015-11-07 21:02:21 -0300

    partial account acty

---
+++ CompetitionPlatform/app/src/main/java/tecno/competitionplatform/activities/AccountActivity.java
@@ -0,0 +1,16 @@
+package tecno.competitionplatform.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import tecno.competitionplatform.activities.R;
+
+public class AccountActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+
+}
